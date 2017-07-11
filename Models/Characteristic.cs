7965239344e37f73a3b07c1864c4865f58aab606
mrtomeq <tomeqc@gmail.com>
2017-07-10 23:38:34 -0400

    Characteristic keys K2222 - K2508

--- Models/Characteristic.cs
+++ Models/Characteristic.cs
@@ -535,6 +535,477 @@ namespace Models
 		[Display(Name = "K2221", Description = "Number Of Trials")]
 		public int NumberOfTrials { get; set; }
 
+		/// <summary>
+		/// K2222 - No. Of Reference Measurements
+		/// </summary>
+		[Display(Name = "K2222", Description = "No. Of Reference Measurements")]
+		public int NoOfReferenceMeasurements { get; set; }
+
+		/// <summary>
+		/// K2225 - Determined Cg Value
+		/// </summary>
+		[Display(Name = "K2225", Description = "Determined Cg Value")]
+		public float DeterminedCgValue { get; set; }
+
+		/// <summary>
+		/// K2226 - Determined Cgk Value
+		/// </summary>
+		[Display(Name = "K2226", Description = "Determined Cgk Value")]
+		public float DeterminedCgkValue { get; set; }
+
+		/// <summary>
+		/// K2226 - Deviation GC Type 3 – GC Type 1
+		/// </summary>
+		[Display(Name = "K2227", Description = "Deviation GC Type 3 – GC Type 1")]
+		public float DeviationGcType3GcType1 { get; set; }
+
+		/// <summary>
+		/// K2228 - Sg Type 1 - Stability
+		/// </summary>
+		[Display(Name = "K2228", Description = "Sg Type 1 - Stability")]
+		public float SgType1Stability { get; set; }
+
+		/// <summary>
+		/// K2243 - Drawing File Name
+		/// </summary>
+		[Display(Name = "K2243", Description = "Drawing File Name")]
+		public string DrawingFileName { get; set; }
+
+		/// <summary>
+		/// K2244 - Drawing Reference Point X
+		/// </summary>
+		[Display(Name = "K2244", Description = "Drawing Reference Point X")]
+		public int DrawingReferencePointX { get; set; }
+
+		/// <summary>
+		/// K2245 - Drawing Reference Point Y
+		/// </summary>
+		[Display(Name = "K2245", Description = "Drawing Reference Point Y")]
+		public int DrawingReferencePointY { get; set; }
+
+		/// <summary>
+		/// K2246 - Drawing Reference Point Z
+		/// </summary>
+		[Display(Name = "K2246", Description = "Drawing Reference Point Z")]
+		public int DrawingReferencePointZ { get; set; }
+
+		/// <summary>
+		/// K2261 - Reference Part number
+		/// </summary>
+		[Display(Name = "K2261", Description = "Reference Part Number")]
+		public string ReferencePartNumberString { get; set; }
+
+		/// <summary>
+		/// K2262 - Reference Part Description
+		/// </summary>
+		[Display(Name = "K2262", Description = "Reference Part Description")]
+		public string ReferencePartDescription { get; set; }
+
+		/// <summary>
+		/// K2263 - Reference Part Actual Value
+		/// </summary>
+		[Display(Name = "K2263", Description = "Reference Part Actual Value")]
+		public float ReferencePartActualValue { get; set; }
+
+		/// <summary>
+		/// K2264 - Reference Part Temperature
+		/// </summary>
+		[Display(Name = "K2264", Description = "Reference Part Temperature")]
+		public float ReferencePartTemperature { get; set; }
+
+		/// <summary>
+		/// K2265 - Reference Part Number (num)
+		/// </summary>
+		[Display(Name = "K2265", Description = "Reference Part Number (num)")]
+		public int ReferencePartNumber { get; set; }
+
+		/// <summary>
+		/// K2266 - Reference Part Serial Number
+		/// </summary>
+		[Display(Name = "K2266", Description = "Reference Part Serial Number")]
+		public string ReferencePartSerialNumber { get; set; }
+
+		/// <summary>
+		/// K2281 - Calibration Part Number Middle
+		/// </summary>
+		[Display(Name = "K2281", Description = "Calibration Part Number Middle")]
+		public string CalibrationPartNumberMiddleString { get; set; }
+
+		/// <summary>
+		/// K2282 - Calibration Part Description Middle
+		/// </summary>
+		[Display(Name = "K2282", Description = "Calibration Part Description Middle")]
+		public string CalibrationPartDescriptionMiddle { get; set; }
+
+		/// <summary>
+		/// K2283 - Calibration Part Actual Value Middle
+		/// </summary>
+		[Display(Name = "K2283", Description = "Calibration Part Actual Value Middle")]
+		public float CalibrationPartActualValueMiddle { get; set; }
+
+		/// <summary>
+		/// K2284 - Calibration Part Temperature Middle
+		/// </summary>
+		[Display(Name = "K2284", Description = "Calibration Part Temperature Middle")]
+		public float CalibrationPartTemperatureMiddle { get; set; }
+
+		/// <summary>
+		/// K2285 - Calibration Part Number (num) Middle
+		/// </summary>
+		[Display(Name = "K2285", Description = "Calibration Part Number (num) Middle")]
+		public int CalibrationPartNumberMiddle { get; set; }
+
+		/// <summary>
+		/// K2286 - Calibration Part Serial Number Middle
+		/// </summary>
+		[Display(Name = "K2286", Description = "Calibration Part Serial Number Middle")]
+		public string CalibrationPartSerialNumberMiddle { get; set; }
+
+		/// <summary>
+		/// K2301 - Machine Number Text
+		/// </summary>
+		[Display(Name = "K2301", Description = "Machine Number Text")]
+		public string MachineNumberText { get; set; }
+
+		/// <summary>
+		/// K2302 - Machine Description
+		/// </summary>
+		[Display(Name = "K2302", Description = "Machine Description")]
+		public string MachineDescription { get; set; }
+
+		/// <summary>
+		/// K2303 - Department / Cost Centre
+		/// </summary>
+		[Display(Name = "K2303", Description = "Department / Cost Centre")]
+		public string DepartmentCostCentre { get; set; }
+
+		/// <summary>
+		/// K2304 - Machine Location
+		/// </summary>
+		[Display(Name = "K2304", Description = "Machine Location")]
+		public string MachineLocation { get; set; }
+
+		/// <summary>
+		/// K2305 - Machine Number
+		/// </summary>
+		[Display(Name = "K2305", Description = "Machine Number")]
+		public int MachineNumber { get; set; }
+
+		/// <summary>
+		/// K2306 - Area / Plant Sector
+		/// </summary>
+		[Display(Name = "K2306", Description = "Area / Plant Sector")]
+		public string AreaPlantSector { get; set; }
+
+		/// <summary>
+		/// K2307 - PTM Number
+		/// </summary>
+		[Display(Name = "K2307", Description = "PTM Number")]
+		public string PtmNumber { get; set; }
+
+		/// <summary>
+		/// K2311 - Production Type Text(Operation)
+		/// </summary>
+		[Display(Name = "K2311", Description = "Production Type Text(Operation)")]
+		public string ProductionTypeTextOperation { get; set; }
+
+
+		/// <summary>
+		/// K2312 - Production Type Description
+		/// </summary>
+		[Display(Name = "K2312", Description = "Production Type Description")]
+		public string ProductionTypeDescription { get; set; }
+
+		/// <summary>
+		/// K2313 - Production Type Number
+		/// </summary>
+		[Display(Name = "K2313", Description = "Production Type Number")]
+		public int ProductionTypeNumber { get; set; }
+
+		/// <summary>
+		/// K2320 - Contract Number
+		/// </summary>
+		[Display(Name = "K2320", Description = "Contract Number")]
+		public string ContractNumber { get; set; }
+
+		/// <summary>
+		/// K2321 - Contractor Number Text
+		/// </summary>
+		[Display(Name = "K2321", Description = "Contractor Number Text")]
+		public string ContractorNumberText { get; set; }
+
+		/// <summary>
+		/// K2322 - Contractor Name
+		/// </summary>
+		[Display(Name = "K2322", Description = "Contractor Name")]
+		public string ContractorName { get; set; }
+
+		/// <summary>
+		/// K2323 - Contractor Number
+		/// </summary>
+		[Display(Name = "K2323", Description = "Contractor Number")]
+		public int ContractorNumber { get; set; }
+
+		/// <summary>
+		/// K2331 - Work Piece Number Text
+		/// </summary>
+		[Display(Name = "K2331", Description = "Work Piece Number Text")]
+		public string WorkPieceNumberText { get; set; }
+
+		/// <summary>
+		/// K2332 - Work Piece Description
+		/// </summary>
+		[Display(Name = "K2332", Description = "Work Piece Description")]
+		public string WorkPieceDescription { get; set; }
+
+		/// <summary>
+		/// K2333 - Work Piece Number
+		/// </summary>
+		[Display(Name = "K2333", Description = "Work Piece Number")]
+		public int WorkPieceNumber { get; set; }
+
+		/// <summary>
+		/// K2341 - Test Plan Number Text
+		/// </summary>
+		[Display(Name = "K2341", Description = "Test Plan Number Text")]
+		public string TestPlanNumberText { get; set; }
+
+		/// <summary>
+		/// K2342 - Test Plan Name
+		/// </summary>
+		[Display(Name = "K2342", Description = "Test Plan Name")]
+		public string TestPlanName { get; set; }
+
+		/// <summary>
+		/// K2343 - Test Plan Creation Date
+		/// </summary>
+		[Display(Name = "K2343", Description = "Test Plan Creation Date")]
+		public DateTime TestPlanCreationDate { get; set; }
+
+		/// <summary>
+		/// K2344 - Test Plan Creator
+		/// </summary>
+		[Display(Name = "K2344", Description = "Test Plan Creator")]
+		public string TestPlanCreator { get; set; }
+
+		/// <summary>
+		/// K2401 - Gage Number Text
+		/// </summary>
+		[Display(Name = "K2401", Description = "Gage Number Text")]
+		public string GageNumberText { get; set; }
+
+		/// <summary>
+		/// K2402 - Gage Description
+		/// </summary>
+		[Display(Name = "K2402", Description = "Gage Description")]
+		public string GageDescription1 { get; set; }
+
+		/// <summary>
+		/// K2403 - Gage Group
+		/// </summary>
+		[Display(Name = "K2403", Description = "Gage Group")]
+		public string GageGroup { get; set; }
+
+		/// <summary>
+		/// K2404 - Gage Resolution
+		/// </summary>
+		[Display(Name = "K2404", Description = "Gage Resolution")]
+		public float GageResolution { get; set; }
+
+		/// <summary>
+		/// K2405 - Gage Number
+		/// </summary>
+		[Display(Name = "K2405", Description = "Gage Number")]
+		public int GageNumber { get; set; }
+
+		/// <summary>
+		/// K2406 - Gage Manufacturer
+		/// </summary>
+		[Display(Name = "K2406", Description = "Gage Manufacturer")]
+		public string GageManufacturer { get; set; }
+
+		/// <summary>
+		/// K2407 - SPC Device Number
+		/// </summary>
+		[Display(Name = "K2407", Description = "SPC Device Number")]
+		public string SpcDeviceNumber { get; set; }
+
+		/// <summary>
+		/// K2408 - SPC Device Description
+		/// </summary>
+		[Display(Name = "K2408", Description = "SPC Device Description")]
+		public string SpcDeviceDescription { get; set; }
+
+		/// <summary>
+		/// K2409 - SPC Device Type
+		/// </summary>
+		[Display(Name = "K2409", Description = "SPC Device Type")]
+		public string SpcDeviceType { get; set; }
+
+		/// <summary>
+		/// K2410 - Test Location
+		/// </summary>
+		[Display(Name = "K2410", Description = "Test Location")]
+		public string TestLocation { get; set; }
+
+		/// <summary>
+		/// K2411 - Test Begin
+		/// </summary>
+		[Display(Name = "K2411", Description = "Test Begin")]
+		public DateTime TestBegin { get; set; }
+
+		/// <summary>
+		/// K2412 - Test End
+		/// </summary>
+		[Display(Name = "K2412", Description = "Test End")]
+		public DateTime TestEnd { get; set; }
+
+		/// <summary>
+		/// K2413 - Gage Graphic
+		/// </summary>
+		[Display(Name = "K2413", Description = "Gage Graphic")]
+		public string GageGraphic { get; set; }
+
+		/// <summary>
+		/// K2415 + Gage Serial Number
+		/// </summary>
+		[Display(Name = "K2415", Description = "Gage Serial Number")]
+		public string GageSerialNumber { get; set; }
+
+		/// <summary>
+		/// K2416 - Display Device
+		/// </summary>
+		[Display(Name = "K2416", Description = "Display Device")]
+		public string DisplayDevice { get; set; }
+
+		/// <summary>
+		/// K2421 - Operator Number Text
+		/// </summary>
+		[Display(Name = "K2421", Description = "Operator Number Text")]
+		public string OperatorNumberText { get; set; }
+
+		/// <summary>
+		/// K2422 - Operator Name
+		/// </summary>
+		[Display(Name = "K2422", Description = "Operator Name")]
+		public string OperatorName { get; set; }
+
+		/// <summary>
+		/// K2423 - Operator Number
+		/// </summary>
+		[Display(Name = "K2423", Description = "Operator Number")]
+		public int OperatorNumber { get; set; }
+
+		/// <summary>
+		/// K2430 - Sampling Type
+		/// </summary>
+		[Display(Name = "K2430", Description = "Sampling Type")]
+		public int SamplingType { get; set; }
+
+		/// <summary>
+		/// K2432 - Individual Value Output
+		/// </summary>
+		[Display(Name = "K2432", Description = "Individual Value Output")]
+		public int IndividualValueOutput { get; set; }
+
+		/// <summary>
+		/// K2434 - Proof Of Process Capability
+		/// </summary>
+		[Display(Name = "K2434", Description = "Proof of Process Capability")]
+		public int ProofOfProcessCapability { get; set; }
+
+		/// <summary>
+		/// K2436 - Test Frequency
+		/// </summary>
+		[Display(Name = "K2436", Description = "Test Frequency")]
+		public string TestFrequency { get; set; }
+
+		/// <summary>
+		/// K2438 - Quantity Tested
+		/// </summary>
+		[Display(Name = "K2438", Description = "Quantity Tested")]
+		public string QuantityTested { get; set; }
+
+		/// <summary>
+		/// K2440 - Assembly Component
+		/// </summary>
+		[Display(Name = "K2440", Description = "Assembly Component")]
+		public string AssemblyComponent { get; set; }
+
+		/// <summary>
+		/// K2442 - Assembly Component Mass
+		/// </summary>
+		[Display(Name = "K2442", Description = "Assembly Component Mass")]
+		public string AssemblyComponentMass { get; set; }
+
+		/// <summary>
+		/// K2444 - Assembly Component Material
+		/// </summary>
+		[Display(Name = "K2444", Description = "Assembly Component Material")]
+		public string AssemblyComponentMaterial { get; set; }
+
+		/// <summary>
+		/// K2446 - Supplier’s Product Description
+		/// </summary>
+		[Display(Name = "K2446", Description = "Supplier’s Product Description")]
+		public string SuppliersProductDescription { get; set; }
+
+		/// <summary>
+		/// K2448 - Assembly Component Manufacturer
+		/// </summary>
+		[Display(Name = "K2448", Description = "Assembly Component Manufacturer")]
+		public string AssemblyComponentManufacturer { get; set; }
+
+		/// <summary>
+		/// K2501 - Dimension Attribute
+		/// </summary>
+		[Display(Name = "K2501", Description = "Dimension Attribute")]
+		public int DimensionAttribute { get; set; }
+
+		/// <summary>
+		/// K2502 - Tolerance Display Format
+		/// </summary>
+		[Display(Name = "K2502", Description = "Tolerance Display Format")]
+		public int ToleranceDisplayFormat { get; set; }
+
+		/// <summary>
+		/// K2503 - Dimension Type
+		/// </summary>
+		[Display(Name = "K2503", Description = "Dimension Type")]
+		public int DimensionType { get; set; }
+
+		/// <summary>
+		/// K2504 - Drawing Change Status
+		/// </summary>
+		[Display(Name = "K2504", Description = "Drawing Change Status")]
+		public int DrawingChangeStatus { get; set; }
+
+		/// <summary>
+		/// K2505 - View Description
+		/// </summary>
+		[Display(Name = "K2505", Description = "View Description")]
+		public string ViewDescription { get; set; }
+
+		/// <summary>
+		/// K2506 - Sheet Number
+		/// </summary>
+		[Display(Name = "K2506", Description = "Sheet Number")]
+		public int SheetNumber { get; set; }
+
+		/// <summary>
+		/// K2507 - Drawing Field Character
+		/// </summary>
+		[Display(Name = "K2507", Description = "Drawing Field Character")]
+		public string DrawingFieldCharacter { get; set; }
+
+		/// <summary>
+		/// Drawing Field Number
+		/// </summary>
+		[Display(Name = "K2508", Description = "Drawing Field Number")]
+		public int DrawingFieldNumber { get; set; }
+
+
+
 
 
 
